16d0056822365ab9675e3d6b0e9d53046e78595b
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-10 14:36:55 +0200

    opens a file and counts the 'a' character, excepts error

--- countas/count-as.py
+++ countas/count-as.py
@@ -1,8 +1,18 @@
-def count_as(change_this):
-    pass
+def count_as(filename):
     # Create a function that takes a filename as string parameter,
     # counts the occurances of the letter "a", and returns it as a number.
     # If the file does not exist, the function should return 0 and not break.
+
+    try:
+        my_file = open(filename, mode='r')
+        text = my_file.read()
+        count = 0
+        for char in text:
+            if char == 'a':
+                count += 1
+        return count
+    except FileNotFoundError:
+        return 0
 
 print(count_as("afile.txt")) # should print 28
 print(count_as("not-a-file")) # should print 0
