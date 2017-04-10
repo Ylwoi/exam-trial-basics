93ddd404daab5fd02b229739abaf365e2033af91
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-10 16:02:43 +0200

    refactored, useless lines deleted

--- box/box.py
+++ box/box.py
@@ -1,9 +1,8 @@
-class Cuboid(object):
-    pass
     # Create a class that represents a cuboid:
     # It should take its three dimensions as constructor parameters (numbers)
     # It should have a method called `get_surface` that returns the cuboid's surface
     # It should have a method called `get_volume` that returns the cuboid's volume
+
 
 class Cuboid():
     def __init__(self, a, b, c):
