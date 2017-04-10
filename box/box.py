a062649a4ab6414f1349a301cf0b8126eee04ed8
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-10 14:32:22 +0200

    a constructor and two methods created in the class

--- box/box.py
+++ box/box.py
@@ -5,6 +5,18 @@ class Cuboid(object):
     # It should have a method called `get_surface` that returns the cuboid's surface
     # It should have a method called `get_volume` that returns the cuboid's volume
 
+class Cuboid():
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def get_surface(self):
+        return 2*(self.a*self.b + self.a*self.c + self.b*self.c)
+
+    def get_volume(self):
+        return self.a*self.b*self.c
+
 box = Cuboid(10, 20, 30)
 print(box.get_surface()) # should print 2200
 print(box.get_volume()) # should print 6000
