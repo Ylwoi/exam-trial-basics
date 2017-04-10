a1288a2ee1e8ec6c2b494b6518e7b7acf12e4c6d
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-10 14:56:22 +0200

    the for loop puts every second element into a list from the origin

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -1,7 +1,13 @@
-def seconds(change_this):
-    pass
+def seconds(num_list):
     # Create a function that takes a list as a parameter,
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
+
+    every_second = []
+    for number in range(len(num_list)):
+        if number % 2 != 0:
+            every_second.append(num_list[number])
+    return every_second
+
 
 print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
